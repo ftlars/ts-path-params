7a53309385cd9dfa9b5128c28c7bafab90370a7b
Lars Ahman <lars.ahman@jaysolutions.com>
2022-11-20 15:16:14 +0200

    test that ci fails on failing test

--- test/replacer.test.ts
+++ test/replacer.test.ts
@@ -25,7 +25,7 @@ describe(setPathParameters.name, () => {
 		test('replace two parameters', () => {
 			const apiPath = '/:id/:name' as const;
 			const replacements: UrlParameters<typeof apiPath, 'id' | 'name'> = {
-				id: '1',
+				id: '2',
 				name: 'lars',
 			};
 			const expectedResult = '/1/lars';
